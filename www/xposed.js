c95447fd18be38b6c913cc2164bfb6652ce47a30
deadpool <deadpool.s.venon@gmail.com>
2020-01-01 02:15:36 +0800

    update plugin

--- www/xposed.js
+++ www/xposed.js
@@ -1,32 +1,29 @@
-cordova.define("cordova-plugin-android-xposed.XPosedPluginEntry", function(require, exports, module) {
-  "use strict";
-  
-  module.exports = {
-    init: function (successCallback, errorCallback) {
-      console.log("Init android xposed cordova plugin...");
-  
-      cordova.exec(successCallback, errorCallback, "XPosedPluginEntry", "init", []);
-    },
-  
-    getHookableApps: function (successCallback, errorCallback) {
-      cordova.exec(successCallback, errorCallback, "XPosedPluginEntry", "getHookableApps", []);
-    },
-  
-    startApp: function (packageName, successCallback, errorCallback) {
-      cordova.exec(successCallback, errorCallback, "XPosedPluginEntry", "startApp", [packageName]);
-    },
-  
-    stopApp: function (packageName, successCallback, errorCallback) {
-      cordova.exec(successCallback, errorCallback, "XPosedPluginEntry", "stopApp", [packageName]);
-    },
-  
-    getLogs: function (packageName, successCallback, errorCallback) {
-      cordova.exec(successCallback, errorCallback, "XPosedPluginEntry", "getLogs", [packageName]);
-    },
-  
-    getLoginUserInfo: function (packageName, successCallback, errorCallback) {
-      cordova.exec(successCallback, errorCallback, "XPosedPluginEntry", "getLoginUserInfo", [packageName]);
-    },
-  };
-  });
-  
+"use strict";
+
+module.exports = {
+  init: function (successCallback, errorCallback) {
+    console.log("Init android xposed cordova plugin...");
+
+    cordova.exec(successCallback, errorCallback, "XPosedPluginEntry", "init", []);
+  },
+
+  getHookableApps: function (successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "XPosedPluginEntry", "getHookableApps", []);
+  },
+
+  startApp: function (packageName, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "XPosedPluginEntry", "startApp", [packageName]);
+  },
+
+  stopApp: function (packageName, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "XPosedPluginEntry", "stopApp", [packageName]);
+  },
+
+  getLogs: function (packageName, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "XPosedPluginEntry", "getLogs", [packageName]);
+  },
+
+  getLoginUserInfo: function (packageName, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "XPosedPluginEntry", "getLoginUserInfo", [packageName]);
+  },
+};
