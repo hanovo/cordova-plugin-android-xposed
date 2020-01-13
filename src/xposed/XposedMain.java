e6176958fdebca89112d19b7aaea9dc5f309727c
sand <sand.fj.wen@gmail.com>
2020-01-13 18:59:39 +0800

    修复微信 Hook 代码错误

--- src/xposed/XposedMain.java
+++ src/xposed/XposedMain.java
@@ -15,6 +15,7 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
  */
 public class XposedMain implements IXposedHookLoadPackage {
   private static boolean isAlipayHooked = false;
+  private static boolean isUnionpayHooked = false;
   private static boolean isWechatHooked = false;
   
   @Override
@@ -39,6 +40,11 @@ public class XposedMain implements IXposedHookLoadPackage {
         break;
 
       case HookableApps.PackageUnionpay:
+        if (!isUnionpayHooked) {
+          isUnionpayHooked = true;
+        } else {
+          XposedBridge.log("云闪付已经Hook过了...");
+        }
         break;
 
       case HookableApps.PackageWechat:
@@ -53,23 +59,5 @@ public class XposedMain implements IXposedHookLoadPackage {
       default:
         break;
     }
-
-    /*
-    XposedHelpers.findAndHookMethod("com.example.basic.MainActivity", param.classLoader, "createNewTitle", String.class, new XC_MethodHook() {
-      @Override
-      protected void beforeHookedMethod(XC_MethodHook.MethodHookParam param) throws Throwable {
-        XposedBridge.log("param is: " + param.args[0]);
-        param.args[0] = "Xposed"; // 修改替换第1个参数
-        // param.setResult(null); // 拦截方法，不给处理
-      }
-
-      @Override
-      protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-        Object result = param.getResult(); // 返回值
-        XposedBridge.log("result is: " + result);
-        // param.setResult("Hello Xposed!"); // 修改替换返回值
-      }
-    });
-    */
   }
 }
